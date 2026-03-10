bbbd87a25530bbd7f6182ef891b4a7100f0a9cc6
papalozarou <github@lozworld.com>
2026-03-10 00:42:24 +0000

    Use parallel micro binary for container healthcheck

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -8,8 +8,8 @@ set -eu
 HEARTBEAT_FILE="${HEARTBEAT_FILE:-/logs/heartbeat.txt}"
 MAX_AGE_SECONDS="${HEALTHCHECK_MAX_AGE_SECONDS:-900}"
 
-command -v microcheck >/dev/null 2>&1
-microcheck --insecure --wait "1s" --cmd "test -f $HEARTBEAT_FILE"
+command -v parallel >/dev/null 2>&1
+parallel --insecure --wait "1s" --cmd "test -f $HEARTBEAT_FILE"
 
 [ -f "$HEARTBEAT_FILE" ]
 
